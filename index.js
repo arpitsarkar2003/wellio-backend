4cf9a356a8ebfc5733af1dd4d86010db9b4e71df
arpit-mac <arpitsarkar16@gmail.com>
2025-11-29 19:59:23 +0530

    Fix: correct author identity

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.use(cors({
   credentials: true,
 }));
 app.use(morgan('combined'));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '70mb' }));
+app.use(express.urlencoded({ extended: true, limit: '70mb' }));
 
 // Import routes
 const authRoutes = require('./src/routes/auth');
